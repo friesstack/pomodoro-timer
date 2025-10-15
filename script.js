70d017c126c702d48b53de007fd07b0c0290b182
Yana Gabrielle <yanagabrielleeee@gmail.com>
2025-10-15 21:37:30 +0800

    final

--- script.js
+++ script.js
@@ -6,32 +6,33 @@ let state = true;
 
 const appTimer = () => {
   const sessionAmount = Number.parseInt(session.textContent);
-
   if (state) {
     state = false;
     let totalSeconds = sessionAmount * 60;
 
     const updateSeconds = () => {
-  const minuteDiv = document.querySelector(".minutes");
-  const secondDiv = document.querySelector(".seconds");
+        const minuteDiv = document.querySelector(".minutes");
+        const secondDiv = document.querySelector(".seconds");
 
-  totalSeconds--;
+        totalSeconds--;
 
-  let minutesLeft = Math.floor(totalSeconds / 60);
-  let secondsLeft = totalSeconds % 60;
+        let minutesLeft = Math.floor(totalSeconds / 60);
+        let secondsLeft = totalSeconds % 60;
 
-  if (secondsLeft < 10) {
-    secondDiv.textContent = "0" + secondsLeft;
-  } else {
-    secondDiv.textContent = secondsLeft;
-  }
-  minuteDiv.textContent = `${minutesLeft}`;
+        if (secondsLeft < 10) {
+        secondDiv.textContent = "0" + secondsLeft;
+        } else {
+        secondDiv.textContent = secondsLeft;
+        }
+        minuteDiv.textContent = `${minutesLeft}`;
 
-  if (minutesLeft === 0 && secondsLeft === 0) {
-    bells.play();
-    clearInterval(myInterval);
-  }
-};
+        if (minutesLeft === 0 && secondsLeft === 0) {
+            bells.play();
+            clearInterval(myInterval);
+            state = true;
+        }
+    };
+
     myInterval = setInterval(updateSeconds, 1000);
   } else {
     alert("Session has already started.");
